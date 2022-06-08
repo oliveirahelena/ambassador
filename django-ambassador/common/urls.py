507bbc5952ac336329e623f7f08cb7725b544e70
Helena Oliveira <helena@informach.com.br>
2022-06-08 20:09:17 -0300

    add admin register

---
+++ django-ambassador/common/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+from .views import RegisterAPIView
+
+urlpatterns = [path("register", RegisterAPIView.as_view())]
--- django-ambassador/common/views.py
+++ django-ambassador/common/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import exceptions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        data = request.data
+
+        if data["password"] != data["password_confirm"]:
+            raise exceptions.APIException("Passwords do not match!")
+
+        data["is_ambassador"] = 0
+
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
