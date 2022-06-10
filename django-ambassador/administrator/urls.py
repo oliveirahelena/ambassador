5c9f00d9aea3ea3589fd40298a4570fb26f5fad3
Helena Oliveira <helena@informach.com.br>
2022-06-10 13:34:07 -0300

    add administrator app

--- django-ambassador/administrator/urls.py
+++ django-ambassador/administrator/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 
-urlpatterns = [path("", include("common.urls"))]
+from .views import AmbassadorAPIView, ProductGenericAPIView, LinkAPIView, OrderAPIView
+
+urlpatterns = [
+    path("", include("common.urls")),
+    path("ambassadors", AmbassadorAPIView.as_view()),
+    path("products", ProductGenericAPIView.as_view()),
+    path("products/<str:pk>", ProductGenericAPIView.as_view()),
+    path("users/<str:pk>/links", LinkAPIView.as_view()),
+    path("orders", OrderAPIView.as_view()),
+]
